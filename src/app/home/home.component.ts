fdf8c3412e3502c6f91d2cfcc615e6ed99d83cb7
Jeff Silva <jeffsilvavillarreal@gmail.com>
2025-02-14 12:21:56 -0500

    Implement Footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HomeHeaderComponent } from "./home-header/home-header.component";
+import { FooterComponent } from "../shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HomeHeaderComponent],
+  imports: [HomeHeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
