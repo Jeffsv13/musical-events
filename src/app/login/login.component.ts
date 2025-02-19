2db0a0d14bcc1eeca47941e0aefdf1e4ef3e5d91
Jeff Silva <jeffsilvavillarreal@gmail.com>
2025-02-18 23:36:38 -0500

    Maquetación del Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { SimpleHeaderComponent } from "../shared/components/simple-header/simple-header.component";
 import { FooterComponent } from "../shared/components/footer/footer.component";
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-login',
-  imports: [SimpleHeaderComponent, FooterComponent],
+  imports: [SimpleHeaderComponent, FooterComponent,ReactiveFormsModule, MatInputModule,MatFormFieldModule,RouterLink,MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
 }
