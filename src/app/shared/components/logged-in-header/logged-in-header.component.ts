c1c4c5a569ff339526ba72451366822dfd782bbe
Jeff Silva <jeffsilvavillarreal@gmail.com>
2025-03-15 00:40:55 -0500

    Implementación del Logged In Header

---
+++ src/app/shared/components/logged-in-header/logged-in-header.component.ts
@@ -0,0 +1,14 @@
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-logged-in-header',
+  imports: [RouterLink, MatButtonModule],
+  templateUrl: './logged-in-header.component.html',
+  styleUrl: './logged-in-header.component.css'
+})
+export class LoggedInHeaderComponent {
+  authService = inject(AuthService);
+}
